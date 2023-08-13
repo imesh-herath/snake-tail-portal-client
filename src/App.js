76ff85a3633c19f8b3c54215555e1d6ccb5ad4d5
imesh.herath <imesh.herath@pickme.lk>
2023-08-13 13:03:48 +0530

    added Login and Signup forms

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-// import logo from "./logo.svg";
-// import Card from "./components/Card";
-// import LogIn from "./components/Login";
-// import SignUp from "./components/SignUp";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <LogIn></LogIn>
-//         {/* <SignUp></SignUp> */}
-//         {/* <a
-//       className="App-link"
-//       href="https://reactjs.org"
-//       target="_blank"
-//       rel="noopener noreferrer"
-//     >
-//       Learn React
-//     </a> */}
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 // import Home from './component/home';
