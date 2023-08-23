eec1873f372c826ad6021f555ed1b81b986c6921
imesh.herath <imesh.herath@pickme.lk>
2023-08-23 12:54:12 +0530

    added firebase DB

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,9 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Appbar from "./Appbar";
+import { db } from "../firebase-config"
+import { collection, getDocs } from "firebase/firestore"
+import moment from "moment/moment";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -29,21 +32,34 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(id, patient_name, snake_image_url, bitten_time, phone_number) {
-  return { id, patient_name, snake_image_url, bitten_time, phone_number };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function CustomizedTables() {
+  const [patients, setPatients] = React.useState([]);
+  const patientsCollectionRef = collection(db, "patients");
+  React.useEffect(() => {
+    const getPatients = async () => {
+      const data = await getDocs(patientsCollectionRef);
+      setPatients(data.docs.map((doc) => ({ ...doc.data(), id: doc.id, data})))
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id, data})))
+    }
+    getPatients()
+  }, [])
+  
+  function createData(id, patient_name, snake_image_url, bitten_time, phone_number) {
+    return { id, patient_name, snake_image_url, bitten_time, phone_number };
+  }
+  
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  ];
+
+
+
   return (
-    <div>
+    <>
       <Appbar />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
@@ -57,20 +73,20 @@ export default function CustomizedTables() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {patients.map((row,index) => (
               <StyledTableRow key={row.id}>
                 <StyledTableCell component="th" scope="row">
-                  {row.id}
+                  {index + 1}
                 </StyledTableCell>
                 <StyledTableCell align="left">{row.patient_name}</StyledTableCell>
                 <StyledTableCell align="left">{row.snake_image_url}</StyledTableCell>
-                <StyledTableCell align="left">{row.bitten_time}</StyledTableCell>
+                <StyledTableCell align="left">{moment(row.bitten_time.seconds * 1000).format("DD MMM YYYY hh:mm a")}</StyledTableCell>
                 <StyledTableCell align="left">{row.phone_number}</StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-    </div>
+    </>
   );
 }
